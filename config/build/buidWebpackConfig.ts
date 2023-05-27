e9647b9118908d7271090730dce5e218672c076c
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2023-05-27 20:11:05 +0300

    feat: configure webpack

---
+++ config/build/buidWebpackConfig.ts
@@ -0,0 +1,28 @@
+import {BuildOptions} from "./types/config";
+import {buildPlugins} from "./buildPlugins";
+import {buildLoaders} from "./buildLoaders";
+import {buildRelolvers} from "./buildRelolvers";
+
+import webpack from "webpack";
+import { buildDevServer } from "./buildDevServer";
+
+export function buidWebpackConfig(options: BuildOptions): webpack.Configuration {
+  const {mode, paths} = options
+
+  return {
+    mode,
+    entry: paths.entry,
+    output: {
+      filename: "[name].[contenthash].js",
+      path: paths.build,
+      clean: true
+    },
+    plugins: buildPlugins(options),
+    module: {
+      rules: buildLoaders()
+    },
+    resolve: buildRelolvers(),
+    devtool: 'inline-source-map',
+    devServer: buildDevServer(options)
+  }
+}
