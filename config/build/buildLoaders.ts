2a1a8987a0212ec4c1c3e12a3acfeda1247d21fb
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2023-05-28 20:48:44 +0300

    feat: add ThemeSwitcher and Button

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,6 +4,11 @@ import { BuildOptions } from "./types/config";
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
 
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ['@svgr/webpack']
+  }
+
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -28,8 +33,19 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     exclude: /node-modules/,
   }
 
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+      },
+    ],
+  }
+
 
   return [
+    fileLoader,
+    svgLoader,
     typescriptLoader,
     cssLoader
   ]
---
+++ src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
@@ -0,0 +1,22 @@
+import React, {FC} from 'react';
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './ThemeSwitcher.module.scss'
+import {Theme, useTheme} from "app/providers/ThemeProvider";
+import LightIcon from '../../assets/icons/theme-light.svg'
+import DarktIcon from '../../assets/icons/theme-dark.svg'
+
+
+
+interface ThemeSwitcherProps {
+  className?: string;
+}
+
+export const ThemeSwitcher: FC<ThemeSwitcherProps> = ({className}) => {
+  const {theme, toggleTheme} = useTheme()
+
+  return (
+    <button className={classNames(cls.ThemeSwitcher, {}, [className])} onClick={toggleTheme}>
+      {theme === Theme.LIGHT ? <LightIcon/> : <DarktIcon/> }
+    </button>
+  );
+};
