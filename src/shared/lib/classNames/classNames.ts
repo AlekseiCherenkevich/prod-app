a40c9f6f680ddcd95b375acaa0f55f8916d3f237
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2023-05-28 18:44:29 +0300

    feat: add Navbar and AppLink

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,9 +1,13 @@
 
 type Mods = Record<string, string| boolean>
 
-export function classNames(cls: string, mods: Mods, additional: string[]): string {
-  return [cls, ...Object.entries(mods)
+export function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
+  return [
+    cls,
+    ...additional.filter(Boolean),
+    ...Object.entries(mods)
     .filter(([className, value]) => Boolean(value))
-    .map(([className, value])=>className), ...additional]
+    .map(([className, value])=>className),
+  ]
     .join(' ')
 }
