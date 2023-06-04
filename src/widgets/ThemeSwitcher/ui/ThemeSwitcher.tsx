a90795c34251237d3229ea232d768e80029bfa40
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2023-06-04 15:35:37 +0300

    feat: add stylelint, add i18n plugin for eslint

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import cls from 'widgets/ThemeSwitcher/ui/ThemeSwitcher.module.scss';
 import { Theme, useTheme } from 'app/providers/ThemeProvider';
 import LightIcon from 'shared/assets/icons/theme-light.svg';
 import DarktIcon from 'shared/assets/icons/theme-dark.svg';
@@ -13,7 +12,7 @@ export const ThemeSwitcher: FC<ThemeSwitcherProps> = ({ className }) => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button type="button" className={classNames(cls.ThemeSwitcher, {}, [className])} onClick={toggleTheme}>
+    <button type="button" className={classNames('', {}, [className])} onClick={toggleTheme}>
       {theme === Theme.LIGHT ? <LightIcon /> : <DarktIcon /> }
     </button>
   );
