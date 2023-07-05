2aca8e3d02e34aaf40818b41b368b122e0b7802c
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2023-07-06 01:49:47 +0300

    fix: fix translation

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,18 +1,22 @@
 import React, { FC } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
   className?: string
 }
 
-export const Navbar: FC<NavbarProps> = ({ className }) => (
-  <div className={classNames(cls.Navbar, {}, [className])}>
+export const Navbar: FC<NavbarProps> = ({ className }) => {
+  const { t } = useTranslation();
+  return (
+    <div className={classNames(cls.Navbar, {}, [className])}>
 
-    <div className={cls.links}>
-      <AppLink to="/" theme={AppLinkTheme.SECONDARY} className={cls.mainLink}>Главная</AppLink>
-      <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>О сайте</AppLink>
+      <div className={cls.links}>
+        <AppLink to="/" theme={AppLinkTheme.SECONDARY} className={cls.mainLink}>{t('Главная')}</AppLink>
+        <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>{t('О сайте')}</AppLink>
+      </div>
     </div>
-  </div>
-);
+  );
+};
